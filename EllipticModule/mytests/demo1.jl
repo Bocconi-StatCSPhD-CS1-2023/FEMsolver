be0c33c39702ee74e392d8e204360ce9b3f0e739
ceciliasecchi <cecilia.secchi98@gmail.com>
2024-01-16 23:05:37 +0100

    added some comments

--- EllipticModule/mytests/demo1.jl
+++ EllipticModule/mytests/demo1.jl
@@ -1,6 +1,5 @@
 using Plots
 
-
 script_dir = @__DIR__
 cd(script_dir)
 
@@ -9,29 +8,37 @@ include(joinpath(@__DIR__, "..", "src\\EllipticModule.jl"))
 import .EllipitcPDE
 const EP = EllipitcPDE
 
+# The meshes available are from mesh0 to mesh4, the amplitude of the triangles that compose them change 
+# (from the biggest to the smallest)
+
 meshdir=joinpath(@__DIR__, "meshes\\mesh2")
 
 #### EXAMPLE 1 ####
 
+# Definition of the equation to solve. Terms not present don't need to be initated as they are missing by default.
+Diff=1
 f(x, y) = sin(π * x) * cos(y * π / 2) * π^2 * 5 / 4  # Forcing function
 
+# boundary conditions
 neuW(y) = π * cos(y * π / 2)  # Neumann in x = -1
 neuE(y) = -π * cos(y * π / 2)  # Neumann in x = 1
 dirS(x) = 0 # Dirichlet in y = 1
 dirN(x) = 0 # Dirichlet in y = -1
 
-coord, NumSol = EP.solution(meshdir; Diff=1, f=f, neuW=neuW, neuE=neuE, dirN=dirN, dirS=dirS)
+# Numerical solution
+coord, NumSol = EP.solution(meshdir;  Diff=Diff, f=f, neuW=neuW, neuE=neuE, dirN=dirN, dirS=dirS)
 plot_numerical = surface(coord[:, 1], coord[:, 2], NumSol, c=:blues, title = "Numerical solution")
 
+# Analytical solution
 ureal(x, y) = sin(π * x) * cos(y * π / 2)  # This is the solution of my PDE
 sol = ureal.(coord[:, 1], coord[:, 2]);
-# Plot Analytical solution
 plot_analytical = surface(coord[:, 1], coord[:, 2], sol, seriestype = :surface, c=:reds, title = "Analytical solution")
 
+# Comparison of the two results
 combined_plot = plot(plot_numerical, plot_analytical, layout=(1, 2))
 display(combined_plot)
 
+# Computation of the L2 norm of the error 
 error=EP.Err_L2_project(meshdir, NumSol, sol)
 println("L2 Error for the given mesh is: $error")
 
-ccc = EP.MassBalance(meshdir, NumSol; Diff=1, f=f, neuW=neuW, neuE=neuE, dirN=dirN, dirS=dirS)
--- EllipticModule/mytests/demo2.jl
+++ EllipticModule/mytests/demo2.jl
@@ -1,6 +1,5 @@
 using Plots
 
-
 script_dir = @__DIR__
 cd(script_dir)
 
@@ -9,10 +8,17 @@ include(joinpath(@__DIR__, "..", "src\\EllipticModule.jl"))
 import .EllipitcPDE
 const EP = EllipitcPDE
 
+# The meshes available are from mesh0 to mesh4, the amplitude of the triangles that compose them change 
+# (from the biggest to the smallest)
 meshdir=joinpath(@__DIR__, "meshes\\mesh2")
 
 #### EXAMPLE 2
 
+Diff = 0.01
+c = 0
+beta = [1,3]
+
+# boundary conditions
 dirN(x) = 1 # Dirichlet in y = -1
 dirE(x) = 0 # Dirichlet in y = 1
 dirS(x) = 0
@@ -20,17 +26,17 @@ function dirW(x)
     return x > 0.3 ? 1 : 0
 end
 
-beta = [1,3]
-delta = 0 ### not using the stabilization UPU
-Diff = 0.01
-c = 0
+# Numerical solution without using the stabilization method UPU
+delta = 0 
 coord, NumSol = EP.solution(meshdir; Diff=Diff, delta=delta, beta=beta, c=c, dirW=dirW, dirE=dirE, dirN=dirN, dirS=dirS)
 plot_numerical = surface(coord[:, 1], coord[:, 2], NumSol, c=:blues, title = "No stabilization")
 
-delta = 0.1 ### using the stabilization UPU
+# Numerical solution using the stabilization method UPU
+delta = 0.1 
 coord, NumSol = EP.solution(meshdir; Diff=Diff, delta=delta, beta=beta, c=c, dirW=dirW, dirE=dirE, dirN=dirN, dirS=dirS)
 plot_numerical_st = surface(coord[:, 1], coord[:, 2], NumSol, c=:blues, title = "With stabilization")
 
+# Comparison of the two results
 combined_plot = plot(plot_numerical, plot_numerical_st, layout=(1, 2))
 display(combined_plot)
 
